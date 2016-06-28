b0621e267e2f6e4f5b078be78828f04dc2a09162
msumalvico <sumalvico@aspra35>
2016-06-28 18:06:30 +0200

    the EM algorithm in soft and hard version

---
+++ algorithms/em.py
@@ -0,0 +1,201 @@
+from collections import defaultdict
+import algorithms.branching
+from algorithms.mcmc import *
+#import algorithms.ngrams
+#import algorithms.optrules
+#import algorithms.align
+#from algorithms.optrules import extract_all_rules
+#from datastruct.lexicon import *
+#from datastruct.rules import *
+#from utils.files import *
+from utils.printer import *
+#import settings
+#import re
+#
+#import math
+
+def hardem(lexicon, model, edges):
+	old_cost = lexicon.cost + model.cost
+	old_num_rules = model.num_rules()
+	iter_num = 0
+	print('num_rules = %d' % old_num_rules)
+	print('cost = %f' % old_cost)
+
+	# main EM loop
+#	while True:
+	while iter_num < settings.EM_MAX_ITERATIONS:
+		iter_num += 1
+		print()
+		print('=== Iteration %d ===' % iter_num)
+		print('Resetting lexicon...')
+		lexicon.reset(model)
+#		lexicon.recompute_cost(model)
+		# compute maximum branching
+		print('Computing maximum branching...')
+		vertices = list(lexicon.iter_nodes())
+		edges = list(e for e in edges if model.has_rule(e.rule))
+		branching = algorithms.branching.branching(vertices, edges)
+		for e in branching:
+			lexicon.add_edge(e)
+		print('intermediate lexicon cost: %f' % lexicon.cost)
+		# fit the model to the optimum lexicon
+		print('Fitting the model...')
+		model.fit_to_lexicon(lexicon)
+		# recompute edge costs
+		for e in edges:
+			if model.has_rule(e.rule):
+				e.cost = model.edge_cost(e)
+			elif lexicon.has_edge(e):
+				lexicon.remove_edge(e)
+		lexicon.recompute_cost(model)
+		# compute iteration statistics and interruption criterion
+		num_rules = model.num_rules()
+		cost = lexicon.cost + model.cost
+		print('num_rules = %d' % num_rules)
+		print('lexicon cost = %f' % lexicon.cost)
+		print('model cost = %f' % model.cost)
+		print('total cost = %f' % cost)
+#		if 0 <= old_cost-cost <= 1 and num_rules == old_num_rules:
+#			break
+#		break # TODO test
+		old_cost = cost
+		old_num_rules = num_rules
+
+def softem(lexicon, model, edges):
+
+	old_cost = lexicon.cost + model.cost
+	old_num_rules = model.num_rules()
+	iter_num = 0
+	print('num_rules = %d' % old_num_rules)
+	print('cost = %f' % old_cost)
+
+	# main EM loop
+#	while True:
+	while iter_num < settings.EM_MAX_ITERATIONS:
+		iter_num += 1
+		print()
+		print('=== Iteration %d ===' % iter_num)
+
+		# init sampler
+		lexicon.reset(model)
+		sampler = MCMCGraphSampler(model, lexicon, edges)
+		sampler.add_stat('acc_rate', AcceptanceRateStatistic(sampler))
+		sampler.add_stat('exp_edge_freq', EdgeFrequencyStatistic(sampler))
+		sampler.add_stat('exp_logl', ExpectedLogLikelihoodStatistic(sampler))
+		print('Warming up the sampler...')
+		pp = progress_printer(settings.SAMPLING_WARMUP_ITERATIONS)
+		for i in pp:
+			sampler.next()
+
+		# sample the graphs
+#		sample_stats = []
+		print('Sampling...')
+		sampler.reset()
+		pp = progress_printer(settings.SAMPLING_ITERATIONS)
+		for i in pp:
+			sampler.next()
+#			sample_stats.append((sampler.lexicon.cost, sampler.stats['acc_rate'].value()))
+		sampler.update_stats()
+
+		# reformat the sampling results
+		lexicon_cost = sampler.stats['exp_logl'].value()
+		print('acceptance rate = %f' % sampler.stats['acc_rate'].value())
+		print('lexicon cost = %f' % lexicon_cost)
+		# TODO print the total cost here?
+		edges_by_rule = defaultdict(lambda: list())
+		for i, edge in enumerate(sampler.edges):
+			edges_by_rule[edge.rule].append(\
+				(edge, sampler.stats['exp_edge_freq'].value(i)))
+
+#		cost = 0.0
+#		for i, edge in enumerate(edges):
+#			cost += edge.cost * sampler.stats['exp_edge_freq'].value(i)
+#		cost += model.null_cost()
+#		print('sample cost before fitting: %f' % cost)
+
+		# write data for reference
+#		null_cost = model.null_cost()
+#		if iter_num == 1:
+#			with open_to_write('costs_sample.txt.%d' % iter_num) as fp:
+#				for cost, acc in sample_stats:
+#					write_line(fp, (cost, acc))
+		with open_to_write('edges_sample.txt.%d' % iter_num) as fp:
+			for rule, edges_list in edges_by_rule.items():
+				for edge, weight in edges_list:
+					write_line(fp, (edge.source.key, edge.target.key, str(edge.rule), str(weight)))
+
+		# fit the model
+		model.fit_to_sample(edges_by_rule)
+		print('model cost = %f' % model.cost)
+		num_rules = model.num_rules()
+		cost = lexicon_cost + model.cost
+		print('num_rules = %d' % num_rules)
+		print('total cost = %f' % cost)
+
+		# recompute the costs
+		edges = [e for e in edges if model.has_rule(e.rule)]
+		for e in edges:
+			e.cost = model.edge_cost(e)
+#		lexicon.recompute_cost(model)
+
+#		cost = 0.0
+#		for i, edge in enumerate(edges):
+#			cost += edge.cost * sampler.stats['exp_edge_freq'].value(i)
+#		cost += model.null_cost()
+#		print('sample cost after fitting: %f' % cost)
+
+		# interruption criterion
+#		if 0 <= old_cost-cost <= 1 and num_rules == old_num_rules:
+#		if (old_cost-cost <= 1 and num_rules == old_num_rules) or\
+#				iter_num >= settings.EM_MAX_ITERATIONS:
+#			break
+		old_cost = cost
+		old_num_rules = num_rules
+
+def load_training_file_with_freq(filename):
+	ruleset = RuleSet()
+	rootdist = algorithms.ngrams.NGramModel(settings.ROOTDIST_N)
+	rootdist.train_from_file(filename)
+	lexicon = Lexicon(rootdist=rootdist, ruleset=ruleset)
+
+	for word, freq in read_tsv_file(filename, (str, int)):
+		lexicon[word] = LexiconNode(word, freq, rootdist.word_prob(word))
+		lexicon.roots.add(word)
+	for word_2, freq, word_1 in read_tsv_file(filename, (str, int, str),\
+			print_progress=True, print_msg='Building lexicon from training data...'):
+		if word_1 != u'-' and word_1 != word_2 and word_2 in lexicon.roots:
+			if word_1 not in lexicon:
+				lexicon.add_word(word_1, 1, rootdist.word_prob(word_1))
+			rule = algorithms.align.align(word_1, word_2).to_string()
+			if rule not in ruleset:
+				ruleset[rule] = RuleData(rule, 1.0, 1, 0)
+			lexicon.draw_edge(word_1, word_2, rule)
+	return lexicon
+
+def load_training_file_without_freq(filename):
+	ruleset = RuleSet()
+	rootdist = algorithms.ngrams.NGramModel(settings.ROOTDIST_N)
+	rootdist.train([(word, 1) for (word,) in read_tsv_file(filename, (str,))])
+	lexicon = Lexicon(rootdist=rootdist, ruleset=ruleset)
+
+	for (word,) in read_tsv_file(filename, (str,)):
+		lexicon[word] = LexiconNode(word, 1, rootdist.word_prob(word))
+		lexicon.roots.add(word)
+	for word_2, word_1 in read_tsv_file(filename, (str, str),\
+			print_progress=True, print_msg='Building lexicon from training data...'):
+		if word_1 != u'-' and word_1 != word_2 and word_2 in lexicon.roots:
+			if word_1 not in lexicon:
+				lexicon.add_word(word_1, 1, rootdist.word_prob(word_1))
+			rule = algorithms.align.align(word_1, word_2).to_string()
+			if rule not in ruleset:
+				ruleset[rule] = RuleData(rule, 1.0, 1, 0)
+			lexicon.draw_edge(word_1, word_2, rule)
+	return lexicon
+
+#TODO supervised
+def load_training_file(filename):
+	if settings.USE_WORD_FREQ:
+		return load_training_file_with_freq(filename)
+	else:
+		return load_training_file_without_freq(filename)
+	
