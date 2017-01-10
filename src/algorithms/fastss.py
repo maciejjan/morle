48dc345940bc0c59058f06d80fff2078c7714ebd
Maciej Sumalvico <macjan@o2.pl>
2017-01-10 12:01:44 +0100

    code cleaning -- remove old commented code: algorithms.fastss, models.point, modules.preprocess

--- src/algorithms/fastss.py
+++ src/algorithms/fastss.py
@@ -11,66 +11,6 @@ MAX_AFFIX_LENGTH = 5
 INFIX_SLOTS = 1
 MAX_OUTPUT_SIZE = 5e10	# 50 GB
 
-## turns a zero into one at the chosen position in a string of zeros and ones
-#def zero_to_one(old_mask, pos):
-#	new_mask = ''
-#	current_pos = 0
-#	for i in range(0, len(old_mask)):
-#		if current_pos == pos:
-#			new_mask += '1'
-#		else:
-#			new_mask += old_mask[i]
-#		if old_mask[i] == '0':
-#			current_pos += 1
-#	return new_mask
-#
-#def mask_correct(mask):
-#	mask_split = re.split('0+', mask)
-#	if len(mask_split) > INFIX_SLOTS+2:
-#		return False
-#	if len(mask_split[0]) > MAX_AFFIX_LENGTH:
-#		return False
-#	if len(mask_split[-1]) > MAX_AFFIX_LENGTH:
-#		return False
-#	if len(mask_split) <= 2: # no alternations
-#		return True
-#	for x in mask_split[1:-1]:
-#		if len(x) > MAX_ALTERNATION_LENGTH:
-#			return False
-#	return True
-#
-## get all substrings of a given word matching the correctness conditions
-#def substrings_for_word(word, wordset):
-#	if settings.USE_TAGS:
-#		word = word[:word.rfind('_')]
-#	substrings = [[(word, '0' * len(word))]]
-#	for i in range(0, min(len(word)//2, MAX_DIST, len(word)-MIN_LENGTH)):
-#		new_substrings = set([])
-#		for s, mask in substrings[i]:
-#			for j in range(0, len(s)):
-#				new_substring = s[:j] + s[j+1:]
-#				new_mask = zero_to_one(mask, j)
-#				if mask_correct(new_mask):
-#					new_substrings.add((new_substring, new_mask))
-#		substrings.append(new_substrings)
-#	result = []
-#	for substring_set in substrings:
-#		result.extend([s for s, mask in substring_set])
-#	# generate substrings for compounding rules
-#	if settings.COMPOUNDING_RULES:
-#		for i in range(1, len(word)-1):
-#			if word[i:] in wordset and len(word[i:]) >= MIN_COMP_LEN:
-#				result.append(word[i:])
-#			if word[:-i] in wordset and len(word[i:]) >= MIN_COMP_LEN:
-#				result.append(word[:-i])
-#			# for words starting with capitalization mark
-#			if '"' + word[i:] in wordset and len(word[i:]) >= MIN_COMP_LEN:
-#				result.append(word[i:])
-#			if '"' + word[:-i] in wordset and len(word[i:]) >= MIN_COMP_LEN:
-#				result.append(word[:-i])
-#	# remove duplicates -- same substrings generated with different masks
-#	return list(set(result))
-
 # TODO compounds
 
 def slice_word(word, slices):
@@ -81,10 +21,7 @@ def slice_word(word, slices):
 		word, slices = queue.pop()
 		if slices in visited: continue
 		visited.add(slices)
-#		print(slices)
-#		results.append(sum((word[i:j] for i, j in slices), ()))
 		substr = sum((word[i:j] for i, j in slices), ())
-#		substr = ''.join(sum((word[i:j] for i, j in slices), ()))
 		results.append(''.join(substr))
 		if len(substr) <= len(word)/2: continue
 		for i, (begin, end) in enumerate(slices):
@@ -114,30 +51,6 @@ def slice_word(word, slices):
 
 def substrings_for_word(word):
 	return set(slice_word(word, ((0, len(word)),)))
-
-# generate substrings for all words
-#def generate_substrings(input_file, output_file, wordset):
-#	with open_to_write(output_file) as fp:
-#		bytes_written = 0
-#		lines_written = 0
-#		print('Generating substrings...')
-#		pp = progress_printer(get_file_size(input_file))
-#		for (word,) in read_tsv_file(input_file, (str,)):
-#			for s in substrings_for_word(word, wordset):
-#				bytes_written += write_line(fp, (len(s), s, word),
-#					count_bytes=True)
-#				lines_written += 1
-#			if bytes_written > MAX_OUTPUT_SIZE:
-#				raise Exception("FastSS: maximum output size exceeded!")
-#			next(pp)
-#	set_file_size(output_file, lines_written)
-#
-#def create_substrings_file(input_file, substrings_file, wordset):
-#	generate_substrings(input_file, substrings_file, wordset)
-#	sort_file(substrings_file, key=2)
-#	sort_file(substrings_file, key=1, numeric=True, reverse=True, stable=True)
-##	sort_file(substrings_file, reverse=True, numeric=True)	# TODO key - umlaut sorting ok?
-#
 
 def generate_substrings(lexicon, print_progress=False):
 	pp = progress_printer(len(lexicon)) if print_progress else None
--- src/modules/preprocess.py
+++ src/modules/preprocess.py
@@ -1,5 +1,3 @@
-# -*- encoding: utf-8 -*-
-
 import algorithms.align
 import algorithms.fastss
 import algorithms.fst
@@ -7,105 +5,9 @@ import algorithms.splrules
 from datastruct.lexicon import *
 from datastruct.rules import *
 from models.point import *
-#import algorithms.cooccurrences
-#from algorithms.ngrams import NGramModel
-#from datastruct.counter import *
-#import utils.db
 from utils.files import *
 from utils.printer import *
 import logging
-#import shared
-#import re
-#import random
-
-#### LOCAL FILTERS ###
-#
-## form: filter(rule, (wordpair))
-#def _lfil_affix_length(rule, wordpair):
-#    affixes = rule.get_affixes()
-#    if not affixes:
-#        return True
-#    if len(affixes) == 1 and not rule.alternations: # exception for single-affix rules
-#        return True
-#    if max([len(a) for a in affixes]) <= settings.MAX_AFFIX_LENGTH:
-#        return True
-#    return False
-#
-#LOCAL_FILTERS = [_lfil_affix_length]
-#
-#def apply_local_filters(rule, wordpair):
-#    for f in LOCAL_FILTERS:
-#        if not f(rule, wordpair):
-#            return False
-#    return True
-#
-#### GLOBAL FILTERS ###
-#
-## form: filter(rule, [list, of, wordpairs...]) 
-#def _gfil_rule_freq(rule, wordpairs):
-#    return len(wordpairs) >= settings.MIN_RULE_FREQ
-#
-#GLOBAL_FILTERS = [_gfil_rule_freq]
-#
-#def apply_global_filters(rule, wordpairs):
-#    for f in GLOBAL_FILTERS:
-#        if not f(rule, wordpairs):
-#            return False
-#    return True
-#
-#### TRAINING DATA ###
-#
-#def load_training_infl_rules(filename):
-#    i_rules_c = {}
-#    print('Loading inflectional rules...')
-#    for rule, ifreq, freq, weight in read_tsv_file(filename):
-#        i_rules_c[rule] = weight
-#    return i_rules_c
-#
-#### RULE EXTRACTING FUNCTIONS ###
-#
-#def extract_rules_from_words(words, substring, outfp, wordset):
-#    pattern = re.compile('(.*)' + '(.*?)'.join([letter for letter in substring]) + '(.*)')
-#    for w1 in words:
-#        for w2 in words:
-#            if w1 < w2:
-#                rule = algorithms.align.extract_rule(w1, w2, pattern)
-#                if rule is not None:
-#                    if apply_local_filters(rule, (w1, w2)):
-#                        write_line(outfp, (w1, w2, rule.to_string()))
-##                    write_line(outfp, (w2, w1, rule.reverse().to_string()))
-#                    elif settings.COMPOUNDING_RULES:
-#                        for i in range(3, min(len(w1), len(w2))):
-#                            if i <= len(rule.prefix[1]) and rule.prefix[1][:i] in wordset:
-#                                write_line(outfp, (w1, w2, rule.to_string()))
-#                                break
-#                            if i <= len(rule.suffix[1]) and rule.suffix[1][-i:] in wordset:
-#                                write_line(outfp, (w1, w2, rule.to_string()))
-#                                break
-#
-#def extract_rules_from_substrings(input_file, output_file, wordset=None):
-#    cur_substr, words = '', []
-#    pp = progress_printer(get_file_size(input_file))
-#    with open_to_write(output_file) as outfp:
-#        for substr, rest in read_tsv_file_by_key(input_file, 2, print_progress=True,\
-#                print_msg='Extracting rules from substrings...'):
-##            words = [(word, freq) for (s_len, word, freq) in rest]
-#            words = [word for (s_len, word) in rest]
-#            extract_rules_from_words(words, substr, outfp, wordset)
-
-#def filter_lemmas(graph_file):
-#    lemmas = set()
-#    for word, freq, base in read_tsv_file('lexicon.full', (str, int, str),\
-#            print_progress=True, print_msg='Loading lemmas...'):
-#        lemmas.add(base)
-#    rename_file('graph.txt', 'graph.txt.unfil')
-#    update_file_size('graph.txt.unfil')
-#    with open_to_write('graph.txt') as outfp:
-#        for word_1, word_2, rule in read_tsv_file('graph.txt.unfil', (str, str, str),\
-#                print_progress=True, print_msg='Filtering...'):
-#            if word_1 in lemmas and word_2 not in lemmas:    # TODO analiza -> analizy not in graph!
-#                write_line(outfp, (word_1, word_2, rule))
-#    update_file_size('graph.txt')
 
 def expand_graph(graph_file):
     '''Annotate graph with additional information needed for filtering:
@@ -186,12 +88,7 @@ def filter_rules(graph_file):
     # cleanup files
 #    rename_file(graph_file, graph_file + '.orig')
     rename_file(graph_file + '.filtered', graph_file)
-#
-#def load_wordset(input_file):
-#    wordset = set([])
-#    for word in read_tsv_file(input_file, (str,)):
-#        wordset.add(word)
-#    return wordset
+
 
 def build_graph(lexicon, graph_file):
     with open_to_write(graph_file) as fp:
@@ -229,15 +126,6 @@ def build_graph_from_training_edges(lexicon, training_file, graph_file):
                     if word_1 not in lexicon:
                         logging.getLogger('main').warning('%s not in lexicon' % word_1)
 
-#def load_edges_by_rule(lexicon, graph_file):
-#    edges_by_rule, rules = defaultdict(lambda: list()), {}
-#    for source, target, rule_str in read_tsv_file(graph_file):
-#        if rule_str not in rules:
-#            rules[rule_str] = Rule.from_string(rule_str)
-#        edges_by_rule[rule_str].append(\
-#            LexiconEdge(lexicon[source], lexicon[target], rules[rule]))
-#    return edges_by_rule, rules
-
 def compute_rule_domsizes(lexicon, rules_file):
     with open_to_write(rules_file + '.tmp') as outfp:
         for rule_str, freq in read_tsv_file(rules_file, (str, int), print_progress=True,\
@@ -247,7 +135,6 @@ def compute_rule_domsizes(lexicon, rules_file):
             write_line(outfp, (rule, freq, domsize))
     rename_file(rules_file + '.tmp', rules_file)
 
-#def split_rules_in_graph(lexicon, trh, graph_file, model):
 def split_rules_in_graph(lexicon, graph_file, model):
     with open_to_write(graph_file + '.spl') as outfp:
         for rule_str, wordpairs in read_tsv_file_by_key(graph_file, key=3,\
@@ -257,7 +144,6 @@ def split_rules_in_graph(lexicon, graph_file, model):
                 for w1, w2 in wordpairs]
             edges_spl =\
                 algorithms.splrules.split_rule(rule_str, edges, lexicon, model)
-#                algorithms.splrules.split_rule(rule_str, edges, trh, model)
             for e in edges_spl:
                 write_line(outfp,\
                     (str(e.source), str(e.target), str(e.rule)))
@@ -280,8 +166,6 @@ def run():
 
     sort_file(shared.filenames['graph'], key=3)
     update_file_size(shared.filenames['graph'])
-#    if settings.LEMMAS_KNOWN:
-#        filter_lemmas(settings.FILES['surface.graph'])
     run_filters(shared.filenames['graph'])
     update_file_size(shared.filenames['graph'])
     aggregate_file(shared.filenames['graph'],\
@@ -297,38 +181,6 @@ def cleanup():
     remove_file_if_exists(shared.filenames['rules'])
     remove_file_if_exists(shared.filenames['lexicon-tr'])
     remove_file_if_exists(shared.filenames['graph'])
-
-#    trh = lexicon.trigram_hash()
-#    print('Fitting model...')
-#    model = fit_model_to_graph(lexicon, settings.FILES['surface.graph'])
-#    print('Splitting rules...')
-#    split_rules_in_graph(lexicon, settings.FILES['surface.graph'], model)
-#    print('Saving model...')
-#    model.save_to_file(settings.FILES['model'])
-
-
-#    wordset = load_wordset(settings.FILES['training.wordlist'])\
-#        if settings.COMPOUNDING_RULES else None
-#    algorithms.fastss.create_substrings_file(\
-#        settings.FILES['training.wordlist'], settings.FILES['surface.substrings'], wordset)
-#    extract_rules_from_substrings(settings.FILES['surface.substrings'],\
-#        settings.FILES['surface.graph'], wordset=wordset)
-#    sort_file(settings.FILES['surface.graph'], key=(1,2), unique=True)
-#    sort_file(settings.FILES['surface.graph'], key=3)
-#    ----
-#    update_file_size(settings.FILES['surface.graph'])
-#    if settings.LEMMAS_KNOWN:
-#        filter_lemmas(settings.FILES['surface.graph'])
-#    filter_rules(settings.FILES['surface.graph'])
-#    update_file_size(settings.FILES['surface.graph'])
-#    aggregate_file(settings.FILES['surface.graph'], settings.FILES['surface.rules'], 3)
-#    rules = RuleSet()
-#    algorithms.optrules.optimize_rules_in_graph(\
-#        settings.FILES['training.wordlist'],\
-#        settings.FILES['surface.graph'],\
-#        settings.FILES['surface.graph'] + '.opt', rules)
-#    rename_file(settings.FILES['surface.graph'] + '.opt', settings.FILES['surface.graph'])
-#    rules.save_to_file('rules.txt.0')
 
 def evaluate():
     print('\nSurface rules: nothing to evaluate.\n')
