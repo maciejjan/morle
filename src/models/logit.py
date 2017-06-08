69d17dabeef19ac8ea60ba0fe81db15acad3c1cf
Maciej Sumalvico <sumalvico@informatik.uni-leipzig.de>
2017-06-08 17:57:46 +0200

    slight changes

--- src/models/logit.py
+++ src/models/logit.py
@@ -1,16 +1,19 @@
-# from datastruct.graph import GraphEdge
+from datastruct.graph import GraphEdge
 # from datastruct.lexicon import LexiconEntry
-# from datastruct.rules import Rule
+from datastruct.rules import Rule
 # from models.generic import Model
 # from utils.files import open_to_write, write_line
 # 
 # from collections import defaultdict
-# from typing import Dict, Iterable, List, Tuple
+
+import numpy as np
+
+from typing import Dict, Iterable, List, Tuple
 
 
 class LogitModel:
 
-    def __init__(self):
+    def __init__(self, edges :Iterable[GraphEdge], rules :Iterable[Rule]):
         self.model_type = 'logit'
 #         Model.__init__(self)
 #         self.edge_costs = {} # type: Dict[GraphEdge, float]
@@ -25,60 +28,76 @@ class LogitModel:
         # TODO initialize the parameters randomly
         # TODO print edges and probabilities
 
-    def add_rule(self, rule :Rule, domsize :int, freq :int = None) -> None:
-        super().add_rule(rule, domsize)
-        # TODO initial coefficients for the rule
-        raise NotImplementedError()
-#         if freq is not None:
-#             self.rule_features[rule][0].fit(freq)
+    # TODO compute slice-wise
 
-    def fit_to_sample(self, sample :Iterable[Tuple[GraphEdge, float]]) -> None:
-        def sample_to_edges_by_rule(sample):
-            edges_by_rule = defaultdict(lambda: list())
-            for edge, weight in sample:
-                edges_by_rule[edge.rule].append((edge, weight))
-            return edges_by_rule
+    def edge_probability(self, slice_size=1000) -> np.ndarray:
+        return expit(np.sum(self.edge_features *\
+                            np.dot(self.rule_selector, 
+                                  self.rule_coefficients),
+                     axis=1))
 
-        edges_by_rule = sample_to_edges_by_rule(sample)
-        # TODO fit the coefficients
-        raise NotImplementedError()
-#         for rule, edges in edges_by_rule.items():
-#             self.rule_features[rule].weighted_fit(\
-#                 self.extractor.extract_feature_values_from_weighted_edges(edges))
+    def gradient(self, y, slice_size=1000) -> np.ndarray:
+        return np.dot(((y-self.edge_prob)*self.rule_selector).transpose(), 
+                      self.edge_features)
 
-    def recompute_edge_costs(self, edges :Iterable[GraphEdge]) -> None:
-        for e in edges:
-            self.edge_costs[e] = self.edge_cost(e)
+    def recompute_edge_costs(self) -> None:
+        self.edge_prob = self.edge_probability()
+        self.edge_costs = np.log(edge_prob) - np.log(1-edge_prob)
 
-    def recompute_root_costs(self, roots :Iterable[LexiconEntry]) -> None:
-        for root in roots:
-            new_cost = self.rootdist.cost_of_change(\
-                    self.extractor.extract_feature_values_from_nodes([root]), [])
-            self.root_costs[root] = new_cost
-
-    def cost_of_change(self, edges_to_add :List[GraphEdge], 
-                       edges_to_remove :List[GraphEdge]) -> float:
-        return sum(self.edge_costs[e] for e in edges_to_add) -\
-                sum(self.root_costs[e.target] for e in edges_to_add) -\
-                sum(self.edge_costs[e] for e in edges_to_remove) +\
-                sum(self.root_costs[e.target] for e in edges_to_remove)
-
-    def apply_change(self, edges_to_add :List[GraphEdge], 
-                     edges_to_remove :List[GraphEdge]) -> None:
-        self.edges_cost += sum(self.edge_costs[e] for e in edges_to_add) -\
-                           sum(self.edge_costs[e] for e in edges_to_remove)
-        self.roots_cost += sum(self.root_costs[e.target] for e in edges_to_remove) -\
-                           sum(self.root_costs[e.target] for e in edges_to_add)
-
-    def edge_cost(self, edge):
-        return self.rule_features[edge.rule].cost(\
-            self.extractor.extract_feature_values_from_edges([edge]))
-
-    def save_rules_to_file(self, filename :str):
-        # TODO save feature vectors
-        with open_to_write(filename) as fp:
-            for rule, features in sorted(self.rule_features.items(),
-                                         reverse=True,
-                                         key=lambda x: x[1][0].trials*x[1][0].prob):
-                write_line(fp, (rule, features.to_string()))
-
+#     def add_rule(self, rule :Rule, domsize :int, freq :int = None) -> None:
+#         super().add_rule(rule, domsize)
+#         # TODO initial coefficients for the rule
+#         raise NotImplementedError()
+# #         if freq is not None:
+# #             self.rule_features[rule][0].fit(freq)
+# 
+#     def fit_to_sample(self, sample :Iterable[Tuple[GraphEdge, float]]) -> None:
+#         def sample_to_edges_by_rule(sample):
+#             edges_by_rule = defaultdict(lambda: list())
+#             for edge, weight in sample:
+#                 edges_by_rule[edge.rule].append((edge, weight))
+#             return edges_by_rule
+# 
+#         edges_by_rule = sample_to_edges_by_rule(sample)
+#         # TODO fit the coefficients
+#         raise NotImplementedError()
+# #         for rule, edges in edges_by_rule.items():
+# #             self.rule_features[rule].weighted_fit(\
+# #                 self.extractor.extract_feature_values_from_weighted_edges(edges))
+# 
+#     def recompute_edge_costs(self, edges :Iterable[GraphEdge]) -> None:
+#         for e in edges:
+#             self.edge_costs[e] = self.edge_cost(e)
+# 
+#     def recompute_root_costs(self, roots :Iterable[LexiconEntry]) -> None:
+#         for root in roots:
+#             new_cost = self.rootdist.cost_of_change(\
+#                     self.extractor.extract_feature_values_from_nodes([root]), [])
+#             self.root_costs[root] = new_cost
+# 
+#     def cost_of_change(self, edges_to_add :List[GraphEdge], 
+#                        edges_to_remove :List[GraphEdge]) -> float:
+#         return sum(self.edge_costs[e] for e in edges_to_add) -\
+#                 sum(self.root_costs[e.target] for e in edges_to_add) -\
+#                 sum(self.edge_costs[e] for e in edges_to_remove) +\
+#                 sum(self.root_costs[e.target] for e in edges_to_remove)
+# 
+#     def apply_change(self, edges_to_add :List[GraphEdge], 
+#                      edges_to_remove :List[GraphEdge]) -> None:
+#         self.edges_cost += sum(self.edge_costs[e] for e in edges_to_add) -\
+#                            sum(self.edge_costs[e] for e in edges_to_remove)
+#         self.roots_cost += sum(self.root_costs[e.target] for e in edges_to_remove) -\
+#                            sum(self.root_costs[e.target] for e in edges_to_add)
+# 
+#     def edge_cost(self, edge):
+#         return self.rule_features[edge.rule].cost(\
+#             self.extractor.extract_feature_values_from_edges([edge]))
+# 
+#     def save_rules_to_file(self, filename :str):
+#         # TODO save feature vectors
+#         with open_to_write(filename) as fp:
+#             for rule, features in sorted(self.rule_features.items(),
+#                                          reverse=True,
+#                                          key=lambda x: x[1][0].trials*x[1][0].prob):
+#                 write_line(fp, (rule, features.to_string()))
+# 
