bcb49adf6990bde7305a4f75701a2f64706a7c0e
Maciej Sumalvico <sumalvico@informatik.uni-leipzig.de>
2017-11-01 16:18:56 +0100

    sampling with block composition: first experiments

--- src/algorithms/negex.py
+++ src/algorithms/negex.py
@@ -1,15 +1,24 @@
 import algorithms.fst
 from datastruct.graph import GraphEdge, EdgeSet
 from datastruct.lexicon import LexiconEntry, Lexicon
-from datastruct.rules import RuleSet
+from datastruct.rules import Rule, RuleSet
 
 from collections import defaultdict
 import hfst
 import logging
 import numpy as np
+from operator import itemgetter
 import random
 import tqdm
 from typing import Tuple
+
+
+def identity_fst():
+    tr = hfst.HfstBasicTransducer()
+    tr.set_final_weight(0, 0.0)
+    tr.add_transition(0, hfst.HfstBasicTransition(0, hfst.IDENTITY, 
+                                                  hfst.IDENTITY, 0))
+    return hfst.HfstTransducer(tr)
 
 
 class NegativeExampleSampler:
@@ -17,17 +26,25 @@ class NegativeExampleSampler:
                  rule_set :RuleSet, edge_set :EdgeSet) -> None:
         self.lexicon = lexicon
         self.lexicon_tr = lexicon_tr
-        self.lexicon_tr.convert(hfst.ImplementationType.HFST_OLW_TYPE)
+#         self.lexicon_tr.convert(hfst.ImplementationType.HFST_OLW_TYPE)
+        self.lexicon_cmp_tr = identity_fst()
+        self.lexicon_cmp_tr.subtract(lexicon_tr)
+        self.lexicon_cmp_tr.convert(hfst.ImplementationType.SFST_TYPE)
         self.rule_set = rule_set
         self.num_pos_ex = defaultdict(lambda: 0)
         for edge in edge_set:
             self.num_pos_ex[edge.rule] += 1
-        self.transducers = { rule : rule.to_fst() for rule in rule_set }
+        self.rule_transducers = [rule.to_fst() for rule in rule_set]
+        self.lex_transducers = [entry.to_fst() for entry in lexicon]
 #         for tr in self.transducers.values():
 #             tr.convert(hfst.ImplementationType.HFST_OLW_TYPE)
+        for tr in self.rule_transducers:
+            tr.convert(hfst.ImplementationType.SFST_TYPE)
+        for tr in self.lex_transducers:
+            tr.convert(hfst.ImplementationType.SFST_TYPE)
 
     def sample(self, sample_size :int) -> Tuple[EdgeSet, np.ndarray]:
-        return self.sample_with_lookup(sample_size)
+        return self.sample_with_block_composition(sample_size)
 
     # TODO works, but is heavily affected by the lookup memory leak
     # TODO start this method in a separate process to circumvent the memory leak
@@ -76,5 +93,61 @@ class NegativeExampleSampler:
         # TODO sort the sample items by rule id
         # TODO for each rule: disjunct all input words, then compose
         #      with the rule automaton
-        raise NotImplementedError()
+
+        BLOCK_SIZE = 1000
+        edge_set = EdgeSet()
+        word_ids = np.array(list(range(len(self.lexicon))))
+
+        def _sample_block(r_id :int, block_size :int) -> EdgeSet:
+            w_ids = np.random.choice(word_ids, size=block_size, replace=False)
+            # sort alphabetically to speedup disjunction
+            w_ids_by_symstr = \
+                list(map(itemgetter(0),
+                         sorted([(int(w_id), self.lexicon[int(w_id)].symstr)\
+                                 for w_id in w_ids],
+                                key=itemgetter(1))))
+            transducers = []
+            tr = algorithms.fst.binary_disjunct(\
+                     [hfst.HfstTransducer(self.lex_transducers[w_id]) \
+                      for w_id in w_ids_by_symstr])
+            tr.compose(self.rule_transducers[r_id])
+            tr.compose(self.lexicon_cmp_tr)
+            tr.minimize()
+            result = EdgeSet()
+            rule = self.rule_set[r_id]
+            for input_, paths in tr.extract_paths().items():
+                input_ = input_.replace(hfst.EPSILON, '')
+                for entry in self.lexicon.get_by_symstr(input_):
+                    for output_, weight in paths:
+                        output_ = output_.replace(hfst.EPSILON, '')
+                        edge = GraphEdge(entry, output_, rule)
+                        if edge not in result:
+                            result.add(edge)
+            return result
+
+        num_edges_for_rule = defaultdict(lambda: 0)
+        progressbar = tqdm.tqdm(total=sample_size)
+        r_id_queue = []
+        while len(edge_set) < sample_size:
+            if not r_id_queue:
+                r_id_queue = list(range(len(self.rule_set)))
+                random.shuffle(r_id_queue)
+            r_id = r_id_queue.pop()
+            rule = self.rule_set[r_id]
+            for edge in _sample_block(r_id, BLOCK_SIZE):
+                if edge not in edge_set:
+                    edge_set.add(edge)
+                    num_edges_for_rule[rule] += 1
+                    progressbar.update()
+                    if len(edge_set) >= sample_size:
+                        break
+        progressbar.close()
+        # compute edge weights
+        weights = np.empty(len(edge_set))
+        for i, edge in enumerate(edge_set):
+            domsize = self.rule_set.get_domsize(edge.rule)
+            num_pos_ex = self.num_pos_ex[edge.rule]
+            num_neg_ex = num_edges_for_rule[edge.rule]
+            weights[i] = (domsize-num_pos_ex) / num_neg_ex
+        return edge_set, weights
 
