added rule costs; computing rule statistics; bugfix: using standard deviation rather than variance in LogNormalEdgeFrequencyModel

src/algorithms/mcmc/samplers.py
@@ -303,16 +303,20 @@ class MCMCGraphSampler:
                                                      for stat in stats]))
 
     def save_rule_stats(self, filename):
-        stats, stat_names = [], []
-        for stat_name, stat in sorted(self.stats.items(), key = itemgetter(0)):
-            if isinstance(stat, RuleStatistic):
-                stat_names.append(stat_name)
-                stats.append(stat)
+        freq, contrib = self.compute_rule_stats()
         with open_to_write(filename) as fp:
-            write_line(fp, ('rule',) + tuple(stat_names))
-            for idx, rule in enumerate(self.rule_set):
-                write_line(fp, (str(rule),) +\
-                               tuple([stat.val[idx] for stat in stats]))
+            for r_id, rule in enumerate(self.model.rule_set):
+                write_line(fp, (rule, freq[r_id], contrib[r_id]))
+#         stats, stat_names = [], []
+#         for stat_name, stat in sorted(self.stats.items(), key = itemgetter(0)):
+#             if isinstance(stat, RuleStatistic):
+#                 stat_names.append(stat_name)
+#                 stats.append(stat)
+#         with open_to_write(filename) as fp:
+#             write_line(fp, ('rule',) + tuple(stat_names))
+#             for idx, rule in enumerate(self.rule_set):
+#                 write_line(fp, (str(rule),) +\
+#                                tuple([stat.val[idx] for stat in stats]))
 
     def save_wordpair_stats(self, filename):
         stats, stat_names = [], []
@@ -346,6 +350,31 @@ class MCMCGraphSampler:
         self.save_edge_stats(shared.filenames['sample-edge-stats'])
         self.save_rule_stats(shared.filenames['sample-rule-stats'])
         self.save_wordpair_stats(shared.filenames['sample-wordpair-stats'])
+
+    def save_root_costs(self, filename):
+        with open_to_write(filename) as fp:
+            for i, entry in enumerate(self.lexicon):
+                write_line(fp, (entry, self.root_cost_cache[i]))
+
+    def save_edge_costs(self, filename):
+        with open_to_write(filename) as fp:
+            for i, edge in enumerate(self.edge_set):
+                write_line(fp, (edge, self.edge_cost_cache[i]))
+
+    def compute_rule_stats(self):
+        # compute the rule statistics (frequency and contribution)
+        # from the expected edge frequencies
+        freq = np.zeros(len(self.model.rule_set))
+        contrib = np.zeros(len(self.model.rule_set))
+        for e_id, edge in enumerate(self.edge_set):
+            e_freq = self.stats['edge_freq'].val[e_id]
+            r_id = self.model.rule_set.get_id(edge.rule)
+            tgt_id = self.lexicon.get_id(edge.target)
+            freq[r_id] += e_freq
+            contrib[r_id] += e_freq * (self.root_cost_cache[tgt_id] - \
+                                       self.edge_cost_cache[e_id])
+        return freq, contrib
+
 
 # TODO constructor arguments should be the same for every type
 #      (pass ensured edges through the lexicon parameter?)

src/models/frequency.py
@@ -4,6 +4,7 @@ from datastruct.rules import RuleSet
 from models.generic import Model, ModelFactory, UnknownModelTypeException
 from utils.files import full_path
 
+import logging
 import numpy as np
 from scipy.stats import norm
 
@@ -62,7 +63,7 @@ class LogNormalEdgeFrequencyModel(EdgeFrequencyModel):
     def __init__(self, rule_set :RuleSet) -> None:
         self.rule_set = rule_set
         self.means = None
-        self.vars = None
+        self.sdevs = None
 
     def fit_rule(self, rule_id :int, freq_vector :np.ndarray,
                  weights :np.ndarray) -> None:
@@ -73,13 +74,13 @@ class LogNormalEdgeFrequencyModel(EdgeFrequencyModel):
             return
         self.means[rule_id,] = np.average(freq_vector, weights=weights)
         err = freq_vector - self.means[rule_id,]
-        self.vars[rule_id,] = np.average(err**2, weights=weights) + 0.001 
+        self.sdevs[rule_id,] = np.sqrt(np.average(err**2, weights=weights)) + 0.001 
 
     def fit(self, edge_set :EdgeSet, weights :np.ndarray) -> None:
         if self.means is None:
             self.means = np.empty(len(self.rule_set))
-        if self.vars is None:
-            self.vars = np.empty(len(self.rule_set))
+        if self.sdevs is None:
+            self.sdevs = np.empty(len(self.rule_set))
         for rule, edge_ids in edge_set.get_edge_ids_by_rule().items():
             edge_ids = tuple(edge_ids)
             freq_vector = np.array([edge_set[i].target.logfreq - \
@@ -92,7 +93,7 @@ class LogNormalEdgeFrequencyModel(EdgeFrequencyModel):
         rule_id = self.rule_set.get_id(edge.rule)
         return -norm.logpdf(edge.target.logfreq-edge.source.logfreq,
                             self.means[rule_id,],
-                            self.vars[rule_id,])
+                            self.sdevs[rule_id,])
 
     def edges_cost(self, edge_set :EdgeSet) -> np.ndarray:
         result = np.zeros(len(edge_set))
@@ -102,19 +103,19 @@ class LogNormalEdgeFrequencyModel(EdgeFrequencyModel):
                                     edge_set[i].source.logfreq \
                                     for i in edge_ids])
             costs = -norm.logpdf(freq_vector, self.means[rule_id,],
-                                 self.vars[rule_id,])
+                                 self.sdevs[rule_id,])
             result[tuple(edge_ids),] = costs
         return result
 
     def save(self, filename :str) -> None:
-        np.savez(full_path(filename), means=self.means, vars=self.vars)
+        np.savez(full_path(filename), means=self.means, sdevs=self.sdevs)
 
     @staticmethod
     def load(filename :str, rule_set :RuleSet) -> 'LogNormalEdgeFrequencyModel':
         result = LogNormalEdgeFrequencyModel(rule_set)
         with np.load(full_path(filename)) as data:
             result.means = data['means']
-            result.vars = data['vars']
+            result.sdevs = data['sdevs']
         return result
 
 

src/models/suite.py
@@ -22,6 +22,10 @@ class ModelSuite:
                  lexicon :Lexicon = None,
                  initialize_models :bool = True) -> None:
         self.rule_set = rule_set
+        self.added_root_cost = shared.config['Models']\
+                                     .getfloat('added_root_cost')
+        self.added_rule_cost = shared.config['Models']\
+                                     .getfloat('added_rule_cost')
         if initialize_models:
             self.root_model = RootModelFactory.create(
                                   shared.config['Models'].get('root_model'))
@@ -68,6 +72,7 @@ class ModelSuite:
         if self.root_feature_model is not None:
             result += self.feature_weight * \
                       self.root_feature_model.root_cost(entry)
+        result += self.added_root_cost
         return result
 
     def roots_cost(self, entries :Union[LexiconEntry, Iterable[LexiconEntry]]) -> np.ndarray:
@@ -83,7 +88,9 @@ class ModelSuite:
         return result
 
     def rule_cost(self, rule :Rule) -> float:
-        return self.edge_model.rule_cost(rule)
+        result = self.edge_model.rule_cost(rule)
+        result += self.added_rule_cost
+        return result
 # 
 #     def edge_cost(self, edge :GraphEdge) -> float:
 #         result = self.edge_model.edge_cost(edge)